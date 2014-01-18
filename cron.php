2579ed2396ed11948a57b4bc76a22cd3ef77373f
maan81 <pranijman@gmail.com>
2014-01-18 15:44:37 +0545

    cron job

---
+++ cron.php
@@ -0,0 +1,30 @@
+<?php
+
+
+//db info
+$db = [
+		'hostname' => 'localhost',
+		'username' => 'root',
+		'password' => 'password',
+		'database' => 'pasteme',
+		'table'    => 'pasteme_table'
+	];
+
+
+
+
+//sql to delete the data which have crossed their days limit
+$sql = 'DELETE FROM '.$db['table'].' WHERE (created_at + INTERVAL d DAY) < NOW()';
+
+
+$con=mysqli_connect($db['hostname'],$db['username'],$db['password'],$db['database']);
+
+// Check connection
+if (mysqli_connect_errno($con)){
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+mysqli_query($con,$sql);
+
+
+mysqli_close($con);
